15a3cb38d12d692f7eab3773142b815d48495b7c
SasaKrstic <krlefcrs@hotmail.com>
2022-09-28 15:10:14 +0200

    Finish

--- src/Board.js
+++ src/Board.js
@@ -1,64 +1,85 @@
-import React, { Component } from "react";
-import Note from "./Note";
+import React, { Component } from 'react'
+import Note from './Note'
 
 class Board extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            notes: [
-                {
-                    id: 0,
-                    note: "Call Atila"
-                },
-                {
-                    id: 1,
-                    note: "Email Branko"
-                },
-                {
-                    id: 2,
-                    note: "Order Printer Ink"
-                }
-            ]
-        }
-        this.eachNote = this.eachNote.bind(this)
-        this.update = this.update.bind(this)
-        this.remove = this.remove.bind(this)
-    }
-    
-    update(newText, i) {
-        console.log('updating item at index', i , newText)
-        this.setState(prevState => ({
-            notes: prevState.notes.map(
-                note => (note.id !== i) ? note : {...note, note: newText}
-            )
-        }))
-    }
-    
-    remove(id) { 
-        console.log('brisanje', id) 
-        this.setState(prevState => ({
-            notes: prevState.notes.filter(note => note.id !== id)
-        }))   
-    }
-    
-    eachNote(note, i) {
-        return (
-            <Note key={i} 
-                  index={i}
-                  onChange={this.update}
-                  onRemove={this.remove}>
-                    {note.note}
-            </Note>
-        )    
-    }
-    
-    render() {
-        return (
-            <div className="board">
-                {this.state.notes.map(this.eachNote)}
-            </div>
-        )
-    }
+	constructor(props) {
+		super(props)
+		this.state = {
+			notes: []
+		}
+		this.add = this.add.bind(this)
+		this.eachNote = this.eachNote.bind(this)
+		this.update = this.update.bind(this)
+		this.remove = this.remove.bind(this)
+		this.nextId = this.nextId.bind(this)
+	}
+
+	componentWillMount() {
+		var self = this
+		if(this.props.count) {
+			fetch(`https://baconipsum.com/api/?type=all-meat&sentences=${this.props.count}`)
+				.then(response => response.json())
+				.then(json => json[0]
+								.split('. ')
+								.forEach(sentence => self.add(sentence.substring(0, 25))))
+		}
+	}
+
+	add(text) {
+		this.setState(prevState => ({
+			notes: [
+				...prevState.notes,
+				{
+					id: this.nextId(),
+					note: text
+				}
+			]
+		}))
+	}
+
+	nextId() {
+		this.uniqueId = this.uniqueId || 0
+		return this.uniqueId++
+	}
+
+	update(newText, i) {
+		console.log('updating item at index', i, newText)
+		this.setState(prevState => ({
+			notes: prevState.notes.map(
+				note => (note.id !== i) ? note : {...note, note: newText}
+			)
+		}))
+	}
+
+	remove(id) {
+		console.log('removing item at', id)
+		this.setState(prevState => ({
+			notes: prevState.notes.filter(note => note.id !== id)
+		}))
+	}
+
+	eachNote(note, i) {
+		return (
+			<Note key={note.id}
+				  index={note.id}
+				  onChange={this.update}
+				  onRemove={this.remove}>
+				  {note.note}
+		    </Note>
+		)
+	}
+
+	render() {
+		return (
+			<div className="board">
+				{this.state.notes.map(this.eachNote)}
+				<button onClick={this.add.bind(null, "New Note")}
+						id="add">
+					plus
+				</button>
+			</div>
+		)
+	}
 }
 
-export default Board;
+export default Board
--- src/Note.js
+++ src/Note.js
@@ -1,59 +1,97 @@
-import React, { Component } from 'react';
-import { FaFlickr, FaPencilAlt, FaTrashAlt } from 'react-icons/fa';
+import React, { Component } from 'react'
+
 
 class Note extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            editing: false
-        }
-        this.edit = this.edit.bind(this)
-        this.save = this.save.bind(this)
-        this.remove = this.remove.bind(this)
-        this.renderForm = this.renderForm.bind(this)
-        this.renderDisplay = this.renderDisplay.bind(this)
-    }
-    edit() {
-        this.setState( {
-            editing: true
-        })
-    }
-    remove() {
-        this.props.onRemove(this.props.index)
-    }
-    save(e) {
-        e.preventDefault();
-        this.props.onChange(this._newText.value, this.props.index)
-        this.setState({ 
-            editing: false
-        })
-    }
-    renderForm() {
-        return (
-            <div className='note'>
-                <form onSubmit={this.save}>
-                    <textarea ref={input => this._newText = input} />
-                    <button id="save"><FaFlickr /></button>
-                </form>
-            </div>
-        )
-    }
-    renderDisplay() {
-        return (
-            <div className='note'> 
-                <p>{this.props.children}</p>
-                <span>
-                    <button onClick={this.edit} id='edit'><FaPencilAlt /></button>
-                    <button onClick={this.remove} id='remove'><FaTrashAlt /></button>
-                </span>
-            </div>
-        ) 
-    }
-     
-    render() {
-        return this.state.editing ? this.renderForm() : this.renderDisplay()
-        //ako je tacno zovi renderForm a ako nije renderDisplay
-    }
+	constructor(props) {
+		super(props)
+		this.state = {
+			editing: false
+		}
+		this.edit = this.edit.bind(this)
+		this.remove = this.remove.bind(this)
+		this.save = this.save.bind(this)
+		this.renderForm = this.renderForm.bind(this)
+		this.renderDisplay = this.renderDisplay.bind(this)
+		this.randomBetween = this.randomBetween.bind(this)
+	}
+	
+	componentWillMount() {
+		this.style = {
+			right: this.randomBetween(0, window.innerWidth - 150, 'px'),
+			top: this.randomBetween(0, window.innerHeight - 150, 'px'),
+			transform: `rotate(${this.randomBetween(-25,25, 'deg')})`
+			
+		}
+	}
+	
+	componentDidUpdate() {
+		var textArea 
+		if(this.state.editing) {
+			textArea = this._newText
+			textArea.focus()
+			textArea.select()
+		}
+	}
+	
+	shouldComponentUpdate(newxProps, nextState) {
+		return (
+			this.props.children !== newxProps.children || this.state !== nextState
+		)
+	}
+	
+	randomBetween(x, y, s) {
+		return x + Math.ceil(Math.random() * (y - x)) + s
+	}
+	edit() {
+		this.setState({
+			editing: true
+		})
+	}
+
+	remove() {
+		this.props.onRemove(this.props.index)
+	}
+
+	save(e) {
+		e.preventDefault()
+		this.props.onChange(this._newText.value, this.props.index)
+		this.setState({
+			editing: false
+		})
+	}
+
+	renderForm() {
+		return (
+			<div className="note" style={this.style}>
+				<form onSubmit={this.save}>
+					<textarea ref={input => this._newText = input}
+						defaultValue={this.props.children}/>
+					<button id="save">ent</button>
+				</form>
+			</div>
+		)
+	}
+
+	renderDisplay() {
+		return (
+			<div className="note"  style={this.style}>
+				<p>{this.props.children}</p>
+				<span>
+					<button onClick={this.edit} id="edit">edit</button>
+					<button onClick={this.remove} id="remove">rem</button>
+				</span>
+			</div>
+		)
+	}
+	render() {
+		return this.state.editing ? this.renderForm() : this.renderDisplay()
+	}
+
 }
 
-export default Note;
+export default Note
+
+
+
+
+
